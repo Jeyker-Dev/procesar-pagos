d93be0bcf12e4ee13fda403f752ed392a4f90944
Jeyker-Dev <mendozajeyker1178@gmail.com>
2025-03-29 20:44:03 -0400

    Updated tailwindcss, flux and fluxpro, changed styles of form

---
+++ app/Http/Controllers/PaymentController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PaymentController extends Controller
+{
+    public function pay(Request $request)
+    {
+
+    }
+
+    public function approval()
+    {
+
+    }
+
+    public function cancelled()
+    {
+
+    }
+}
