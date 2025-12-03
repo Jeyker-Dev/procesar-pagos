f7e0701911ed15b9a4c10cf74112cddb894aa141
Jeyker <mendozajeyker1178@gmail.com>
2025-12-03 16:21:38 -0400

    Add models and relationships for Plan and Subscription

--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Plan extends Model
 {
-    //
+    protected $fillable = [
+        'slug',
+        'price',
+        'duration_days',
+    ];
+
+    public function subscriptions(): HasMany
+    {
+        return $this->hasMany(Subscription::class);
+    }
 }
--- app/Models/Subscription.php
+++ app/Models/Subscription.php
@@ -3,8 +3,27 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Subscription extends Model
 {
-    //
+    protected $fillable = [
+        'active_until',
+        'user_id',
+        'plan_id',
+    ];
+
+    protected array $dates = [
+        'active_until',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function plan(): BelongsTo
+    {
+        return $this->belongsTo(Plan::class);
+    }
 }
